5a32b705142b33c9033b655774abcccd96f26c96
duchuynh1201 <102602729+duchuynh1201@users.noreply.github.com>
2025-01-15 11:44:10 +0700

    Handle search with filter

--- frontend-reactjs/src/components/busList.jsx
+++ frontend-reactjs/src/components/busList.jsx
@@ -36,7 +36,7 @@ const BusList = () => {
   const [busTickets, setBusTickets] = useState([]);
 
   useEffect(() => {
-    const fetchData = async (reset = false) => {
+    const fetchData = async () => {
       try {
         const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/bus-operator/list/0/1000`);
         const data = await response.json();
@@ -44,6 +44,7 @@ const BusList = () => {
         for (const item of data.data) {
           optionTmp.push({ value: item.id, label: item.name });
         }
+        optionTmp.unshift({ value: "", label: "All" });
         setOptions(optionTmp);
       } catch (error) {
         console.error("Error fetching bus operators:", error);
@@ -52,50 +53,59 @@ const BusList = () => {
       console.log("LOADMORE~~~");
       // const checkAuthen = await isAuthenticated();
 
-      const busTypeText = ["Limousine", "Normal Seat", "Sleeper Bus"];
-      const typeOfSeat = document.querySelector(
-        'input[name="typeOfSeat"]:checked'
-      ).value;
-      const price = document.querySelector("#filter-pricing").value;
-      const deparature = getUrlParameter("startPoint");
-      const destination = getUrlParameter("endPoint");
-      const date = getUrlParameter("startTime");
+      loadMore();
+    };
 
-      if (page === 0) {
-        document.getElementById("load-more").style.display = "block";
-      } else {
-        document.getElementById("load-more").style.display = "none";
-      }
+    fetchData();
+    // loadMore();
+  }, []);
 
-      fetch(`${import.meta.env.VITE_BACKEND_URL}/bus/search`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          startPoint: deparature,
-          endPoint: destination,
-          page,
-          limit: 10,
-          boId: busOperator === "" ? undefined : busOperator,
-          type: parseInt(typeOfSeat),
-          price: parseInt(price),
-          startTime: date,
-        }),
-      })
+  const loadMore = async (reset = false) => {
+    const busTypeText = ["Limousine", "Normal Seat", "Sleeper Bus"];
+    const typeOfSeat = document.querySelector(
+      'input[name="typeOfSeat"]:checked'
+    ).value;
+    const price = document.querySelector("#filter-pricing").value;
+    const deparature = getUrlParameter("startPoint");
+    const destination = getUrlParameter("endPoint");
+    const date = getUrlParameter("startTime");
+
+    if (page === 0) {
+      document.getElementById("load-more").style.display = "block";
+    } else {
+      document.getElementById("load-more").style.display = "none";
+    }
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/bus/search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        startPoint: deparature,
+        endPoint: destination,
+        page,
+        limit: 10,
+        boId: busOperator === "" ? undefined : busOperator,
+        type: parseInt(typeOfSeat),
+        price: parseInt(price),
+        startTime: date,
+      }),
+    })
       .then(response => response.json())
       .then(data => {
         if (data.data.length === 0 && reset === false) {
           document.getElementById("load-more").style.display = "none";
           return;
         }
-
+        else if (data.data.length === 0 && reset === true) {
+          document.getElementById("load-more").style.display = "none";
+        }
         let tmpTicket = [];
         for (const item of data.data) {
           let duration =
             (new Date(item.end_time) - new Date(item.start_time)) / 1000;
-          let bus_operator_rating =
-            Math.round(item.averageReviews * 100) / 100;
+          let bus_operator_rating = Math.round(item.averageReviews * 100) / 100;
           let start_point_time =
             new Date(item.start_time).getHours() +
             ":" +
@@ -127,116 +137,21 @@ const BusList = () => {
             duration: secondsToHms(duration),
             type: busTypeText[item.type],
           });
-
-          // html += `
-          //   <div className='mb-4 card bus-ticket'>
-          //     <div className='card-body'>
-          //     <div className='row'>
-          //       <div className='col-md-3'>
-          //       <img
-          //         src='${item.image_url}'
-          //         className='img-fluid rounded'
-          //         alt='...'
-          //       />
-          //       </div>
-          //       <div className='col-md-9'>
-          //       <div className='row'>
-          //         <div className='col-md-9'>
-          //         <h5 className='card-title fw-bold text-black-40'>
-          //           ${item.bus_operators.name}
-          //           <span className='ml-2 badge text-bg-warning'>
-          //           ${Math.round(item.averageReviews * 100) / 100}*
-          //           </span>
-          //         </h5>
-          //         <div className='row'>
-          //           <div className='col-md-6'>
-          //           <h5 className='text-success'>
-          //             ${new Date(item.start_time).getHours()}:
-          //             ${new Date(item.start_time).getSeconds()}
-          //           </h5>
-          //           <p className='text-black-50 mb-0'>
-          //             ${new Date(item.start_time).toISOString().split("T")[0]}
-          //           </p>
-          //           <p className='text-black-50 fw-bold'>
-          //             ${item.start_point.name}
-          //           </p>
-          //           </div>
-          //           <div className='col-md-6'>
-          //           <h5 className='text-success'>
-          //             ${new Date(item.end_time).getHours()}:
-          //             ${new Date(item.end_time).getSeconds()}
-          //           </h5>
-          //           <p className='text-black-50 mb-0'>
-          //             ${new Date(item.end_time).toISOString().split("T")[0]}
-          //           </p>
-          //           <p className='text-black-50 fw-bold'>
-          //             ${item.end_point.name}
-          //           </p>
-          //           </div>
-          //         </div>
-          //         </div>
-          //       <div className='col-md-3 text-end'>
-          //       <h5 className='fw-bold text-primary'>
-          //         ${numberWithThoundsand(item.price)} đ
-          //       </h5>
-          //       <p className='mb-0'>
-          //         <small className='text-muted'>
-          //         ${item.left_seats} seats available
-          //         </small>
-          //       </p>
-          //       </div>
-          //       </div>
-          //       <div className='mt-3 d-flex align-items-center justify-content-between'>
-          //       <div>
-          //         <button
-          //         type='button'
-          //         className='btn btn-success'
-          //         data-bs-toggle='modal'
-          //         data-bs-target='#exampleModal'
-          //         >
-          //         Details
-          //         </button>
-          //       </div>
-          //       <div>
-          //       <span className='text-primary fw-bold'>
-          //       ${secondsToHms(duration)}
-          //       </span>
-          //       <span className='text-black-50'> | </span>
-          //       <span className='text-primary fw-bold'>
-          //       ${busTypeText[item.type]}
-          //       </span>
-          //         </div>
-          //         <div>
-          //       <button
-          //         type='button'
-          //         className='btn btn-primary book-bus book-btn'
-          //         bid=${item.id}
-          //       >
-          //       Book
-          //       </button>
-          //     </div>
-          //       </div>
-          //       </div>
-          //     </div>
-          //     </div>
-          //   </div>
-          //   `;
         }
         // console.log("ticket: ", tmpTicket);
         setBusTickets(tmpTicket);
-
       })
       .catch(error => {
         console.error("Error fetching bus data:", error);
       });
-    };
-
-    fetchData();
-    // loadMore();
-  }, []);
+  };
 
   const handlePricingChange = event => {
     setPricing(parseInt(event.target.value, 10)); // Parse to integer
+  };
+  const handleSubmitFilter = () => {
+    page = 0;
+    loadMore(true);
   };
 
   const formatCurrency = amount => {
@@ -418,7 +333,7 @@ const BusList = () => {
                   <button
                     id="filter"
                     className="btn btn-primary float-center btnLogin"
-                    // onClick={handleSubmitFilter}
+                    onClick={() => handleSubmitFilter()}
                   >
                     Submit
                   </button>
